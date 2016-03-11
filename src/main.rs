93a6b1f34484f2db885c90048ce91f4f4b603873
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2016-03-11 18:40:29 +0800

    Add exit codes

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,57 @@ extern crate slack;
 pub mod send;
 
 use std::io;
+use std::process::exit;
 
 use argparse::{ArgumentParser, StoreTrue, Store};
 
 fn main() {
     let mut input = String::new();
-    if let Ok(_) = io::stdin().read_line(&mut input) {
-        let mut verbose = false;
-        let mut channel = "slackbot".to_string();
-        let mut prepend = "".to_string();
-        let mut append = "".to_string();
-
-        {
-            let mut ap = ArgumentParser::new();
-            ap.set_description("Pipe text into Slack.");
-            ap.refer(&mut verbose)
-                .add_option(&["-v", "--verbose"], StoreTrue,
-                            "Be verbose");
-            ap.refer(&mut channel)
-                .add_option(&["-c", "--channel"], Store,
-                            "Channel to send message to");
-            ap.refer(&mut prepend)
-                .add_option(&["-p", "--prepend"], Store,
-                            "Text to prepend to input on message");
-            ap.refer(&mut append)
-                .add_option(&["-a", "--append"], Store,
-                            "Text to append to input on message");
-            ap.parse_args_or_exit();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            run_command(input);
+            exit(0)
+        },
+        Err(_) => {
+            exit(1)
         }
+    }
+}
 
-        let complete_text = format!("{}{}{}", prepend, input.trim(), append);
+fn run_command(input: String) {
+    let mut verbose = false;
+    let mut channel = "slackbot".to_string();
+    let mut prepend = "".to_string();
+    let mut append = "".to_string();
 
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Pipe text into Slack.");
+        ap.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue,
+                        "Be verbose");
+        ap.refer(&mut channel)
+            .add_option(&["-c", "--channel"], Store,
+                        "Channel to send message to");
+        ap.refer(&mut prepend)
+            .add_option(&["-p", "--prepend"], Store,
+                        "Text to prepend to input on message");
+        ap.refer(&mut append)
+            .add_option(&["-a", "--append"], Store,
+                        "Text to append to input on message");
+        ap.parse_args_or_exit();
+    }
+
+    let complete_text = format!("{}{}{}", prepend, input.trim(),
+                                append);
+
+    if verbose {
+        println!("{}: {}", &channel, &complete_text);
+    }
+
+    if let Err(error) = send::send(&complete_text, &channel) {
         if verbose {
-            println!("{}: {}", &channel, &complete_text);
+            println!("{}", error);
         }
-
-        if let Err(error) = send::send(&complete_text, &channel) {
-            if verbose {
-                println!("{}", error);
-            }
-        }
-    };
+    }
 }
