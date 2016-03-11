46bef13679da6b93c4a68abb9e9e05b77caab5c0
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2016-03-11 16:32:09 +0800

    Use argparse

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,54 @@
+extern crate argparse;
 extern crate slack;
 
-pub mod read;
 pub mod send;
 
+use std::env;
+use std::io;
+
+use argparse::{ArgumentParser, StoreTrue, Store};
+use slack::RtmClient;
+
+use send::{Msg, Sender};
+
 fn main() {
+    let mut input = String::new();
+    if let Ok(_) = io::stdin().read_line(&mut input) {
+        let mut verbose = false;
+        let mut channel = "slackbot".to_string();
+        let mut prepend = "".to_string();
+        let mut append = "".to_string();
+
+        {
+            let mut ap = ArgumentParser::new();
+            ap.set_description("Pipe text into Slack.");
+            ap.refer(&mut verbose)
+                .add_option(&["-v", "--verbose"], StoreTrue,
+                            "Be verbose");
+            ap.refer(&mut channel)
+                .add_option(&["-c", "--channel"], Store,
+                            "Channel to send message to");
+            ap.refer(&mut prepend)
+                .add_option(&["-p", "--prepend"], Store,
+                            "Text to prepend to input on message");
+            ap.refer(&mut append)
+                .add_option(&["-a", "--append"], Store,
+                            "Text to append to input on message");
+            ap.parse_args_or_exit();
+        }
+
+        let complete_text = format!("{}{}{}", prepend, input.trim(), append);
+
+        if verbose {
+            println!("{}: {}", &channel, &complete_text);
+        }
+
+        let msg = send::Msg {
+            text: complete_text,
+            channel: channel,
+        };
+
+        let sender = send::Sender::new(RtmClient::new("token"));
+        sender.send(msg);
+    };
 }
--- src/send.rs
+++ src/send.rs
@@ -1,30 +1,23 @@
 use slack::RtmClient;
 
 pub struct Msg {
-    text: String,
-    send_to: Option<String>,
-    prepend_text: Option<String>,
-    append_text: Option<String>,
+    pub text: String,
+    pub channel: String,
 }
 
-impl Msg {
-    fn final_msg(&self) -> String {
-        format!("{}{}{}",
-                self.prepend_text.clone().unwrap_or("".to_string()),
-                self.text.clone(),
-                self.append_text.clone().unwrap_or("".to_string()))
-    }
-}
-
-struct Sender {
+pub struct Sender {
     client: RtmClient,
 }
 
 impl Sender {
+    pub fn new(client: RtmClient) -> Sender {
+        Sender { client: client }
+    }
+
     pub fn send(&self, msg: Msg) -> Result<(), String> {
         match self.find_channel_id(&msg) {
             Some(channel_id) => {
-                self.client.send_message(&channel_id, &msg.final_msg());
+                self.client.send_message(&channel_id, &msg.text.clone());
                 Ok(())
             },
             None => Err("channel does not exist".to_string())
@@ -32,14 +25,10 @@ impl Sender {
     }
 
     fn find_channel_id(&self, msg: &Msg) -> Option<String> {
-        if let Some(ref to) = msg.send_to {
-            self.client
-                .get_channels()
-                .iter()
-                .find(|channel| channel.name == to.to_owned())
-                .map(|channel| channel.id.clone())
-        } else {
-            None
-        }
+        self.client
+            .get_channels()
+            .iter()
+            .find(|channel| channel.name == msg.channel.to_owned())
+            .map(|channel| channel.id.clone())
     }
 }
