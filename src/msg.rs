d12fac1d02da133b43bedace7148f411ece15350
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2016-03-16 11:06:44 -0400

    Use traits to refactor get_channel_addr

--- src/msg.rs
+++ src/msg.rs
@@ -17,31 +17,67 @@ impl Msg {
 
     pub fn send(&self) -> Result<(), String> {
         let mut client = slack::RtmClient::new(&self.token);
-        try!(client.login().map_err(|_| "unable to login to slack".to_string()));
-        let channel_addr = try!(self.find_channel_addr(&client));
         try!(client
-             .post_message(&channel_addr, &self.text.clone(), None)
+             .login()
+             .map_err(|_| "unable to login to slack".to_string()));
+        let addr = try!(self.find_addr(client.get_channels())
+                        .or(self.find_addr(client.get_users()))
+                        .or(self.find_addr(client.get_groups()))
+                        .ok_or("unable to find channel".to_string()));
+        try!(client
+             .post_message(&addr, &self.text.clone(), None)
              .map_err(|_| "unable to send message".to_string()));
         Ok(())
     }
 
-    fn find_channel_addr(&self, client: &slack::RtmClient)
-            -> Result<String, String> {
-        client
-            .get_channels()
+    fn find_addr<T: HasName + HasId>(&self, recipients: Vec<T>)
+            -> Option<String> {
+        recipients
             .iter()
-            .find(|c| c.name.to_string() == self.channel)
-            .map(|r| r.id.clone())
-            .or(client
-                 .get_users()
-                 .iter()
-                 .find(|u| u.name.to_string() == self.channel)
-                 .map(|r| r.id.clone()))
-             .or(client
-                 .get_groups()
-                 .iter()
-                 .find(|g| g.name.to_string() == self.channel)
-                 .map(|r| r.id.clone()))
-             .ok_or("unable to find channel".to_string())
+            .find(|x| x.name() == self.channel)
+            .map(|x| x.id())
+    }
+}
+
+trait HasId {
+    fn id(&self) -> String;
+}
+
+impl HasId for slack::Channel {
+    fn id(&self) -> String {
+        self.id.to_string()
+    }
+}
+impl HasId for slack::User {
+    fn id(&self) -> String {
+        self.id.to_string()
+    }
+}
+
+impl HasId for slack::Group {
+    fn id(&self) -> String {
+        self.id.to_string()
+    }
+}
+
+trait HasName {
+    fn name(&self) -> String;
+}
+
+impl HasName for slack::Channel {
+    fn name(&self) -> String {
+        self.name.to_string()
+    }
+}
+
+impl HasName for slack::User {
+    fn name(&self) -> String {
+        self.name.to_string()
+    }
+}
+
+impl HasName for slack::Group {
+    fn name(&self) -> String {
+        self.name.to_string()
     }
 }
